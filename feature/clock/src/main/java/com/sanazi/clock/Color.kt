7e66a568528557ca31dd4c77b8a7399a5673f9df
Drozd_AS <31595916+SD-SanaZI@users.noreply.github.com>
2025-12-15 22:42:58 +0300

    add clock view

---
+++ feature/clock/src/main/java/com/sanazi/clock/Color.kt
@@ -0,0 +1,11 @@
+package com.sanazi.clock
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGrey = Color(0xFF404040)
+val Grey = Color(0xFF707070)
+val LightGrey = Color(0xFF909090)
+val White = Color.White
+val SecondColor = Color.Blue
+val MinuteColor = Color.White
+val HourColor = Color.White
